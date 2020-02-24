e793689d50f4654afc7f3dfc85bf2f4620c88562
angel <acanales199@gmail.com>
2020-02-23 20:33:49 -0600

    nuevos cambios por smart asp

---
+++ SQLQuery2.sql
@@ -0,0 +1,8 @@
+﻿	  USE [aspnet-VEE-2020];
+GO
+BACKUP DATABASE [aspnet-VEE-2020]
+TO DISK = 'C:\Users\canales\source\repos\VEE\VEE\App_Data\aspnet-VEE-2020.Bak'
+   WITH FORMAT,
+      MEDIANAME = 'Z_SQLServerBackups',
+      NAME = 'Full Backup of aspnet-VEE-2020';
+GO
--- VEE/App_Start/BundleConfig.cs
+++ VEE/App_Start/BundleConfig.cs
@@ -21,39 +21,39 @@ namespace VEE
 
             bundles.Add(new ScriptBundle("~/Scripts/").Include(
                   
-                     "~/Scripts/plugins/jquery/jquery.min.js",
-                    "~/Scripts/plugins/bootstrap/js/bootstrap.bundle.min.js",
-                    "~/Scripts/plugins/chartjs-old/Chart.min.js",
-                    "~/Scripts/plugins/fastclick/fastclick.js",
-                    "~/Scripts/plugins/morris/morris.min.js",
-                    "~/Scripts/plugins/sparkline/jquery.sparkline.min.js",
-                    "~/Scripts/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js",
-                    "~/Scripts/plugins/jvectormap/jquery-jvectormap-world-mill-en.js",
-                    "~/Scripts/plugins/knob/jquery.knob.js",
-                    "~/Scripts/plugins/daterangepicker/daterangepicker.js",
-                    "~/Scripts/plugins/datepicker/bootstrap-datepicker.js",
-                    "~/Scripts/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js",
-                    "~/Scripts/plugins/slimScroll/jquery.slimscroll.min.js",
-                    "~/Scripts/plugins/fastclick/fastclick.js",
-                    "~/Scripts/dist/js/adminlte.js",
-                    "~/Scripts/dist/js/pages/dashboard.js",
-                    "~/Scripts/dist/js/demo.js",
-                    "~/Scripts/plugins/flot/jquery.flot.min.js",
-                    "~/Scripts/plugins/flot/jquery.flot.resize.min.js",
-                    "~/Scripts/plugins/flot/jquery.flot.pie.min.js",
-                    "~/Scripts/plugins/flot/jquery.flot.categories.min.js"
+                    // "~/Scripts/plugins/jquery/jquery.min.js",
+                    //"~/Scripts/plugins/bootstrap/js/bootstrap.bundle.min.js",
+                    //"~/Scripts/plugins/chartjs-old/Chart.min.js",
+                    //"~/Scripts/plugins/fastclick/fastclick.js",
+                    //"~/Scripts/plugins/morris/morris.min.js",
+                    //"~/Scripts/plugins/sparkline/jquery.sparkline.min.js",
+                    //"~/Scripts/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js",
+                    //"~/Scripts/plugins/jvectormap/jquery-jvectormap-world-mill-en.js",
+                    //"~/Scripts/plugins/knob/jquery.knob.js",
+                    //"~/Scripts/plugins/daterangepicker/daterangepicker.js",
+                    //"~/Scripts/plugins/datepicker/bootstrap-datepicker.js",
+                    //"~/Scripts/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js",
+                    //"~/Scripts/plugins/slimScroll/jquery.slimscroll.min.js",
+                    //"~/Scripts/plugins/fastclick/fastclick.js",
+                    //"~/Scripts/dist/js/adminlte.js",
+                    //"~/Scripts/dist/js/pages/dashboard.js",
+                    //"~/Scripts/dist/js/demo.js",
+                    //"~/Scripts/plugins/flot/jquery.flot.min.js",
+                    //"~/Scripts/plugins/flot/jquery.flot.resize.min.js",
+                    //"~/Scripts/plugins/flot/jquery.flot.pie.min.js",
+                    //"~/Scripts/plugins/flot/jquery.flot.categories.min.js"
                     ));
 
                                        bundles.Add(new StyleBundle("~/Scripts").Include(
                       
-                      "~/Scripts/plugins/font-awesome/css/font-awesome.min.css",
-                      "~/Scripts/dist/css/adminlte.min.css",
-                      "~/Scripts/plugins/iCheck/flat/blue.css",
-                      "~/Scripts/plugins/morris/morris.css",
-                      "~/Scripts/plugins/jvectormap/jquery-jvectormap-1.2.2.css",
-                      "~/Scripts/plugins/datepicker/datepicker3.css",
-                      "~/Scripts/plugins/daterangepicker/daterangepicker-bs3.css"
-                     , "~/Scripts/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css"
+                     // "~/Scripts/plugins/font-awesome/css/font-awesome.min.css",
+                     // "~/Scripts/dist/css/adminlte.min.css",
+                     // "~/Scripts/plugins/iCheck/flat/blue.css",
+                     // "~/Scripts/plugins/morris/morris.css",
+                     // "~/Scripts/plugins/jvectormap/jquery-jvectormap-1.2.2.css",
+                     // "~/Scripts/plugins/datepicker/datepicker3.css",
+                     // "~/Scripts/plugins/daterangepicker/daterangepicker-bs3.css"
+                     //, "~/Scripts/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css"
                       ));
    
          
